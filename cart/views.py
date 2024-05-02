84c545da84f3a581231ed4cb4e8fe34dd513e4b8
Md-Monirul-Islam <md.monirulmunna.cse@gmail.com>
2024-05-02 20:48:28 +0600

    Implement data info into cart and calculate tax,cart increment,decrement and cart item remove

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from cart.models import Cart, CartItem
@@ -37,6 +37,28 @@ def add_cart(request, product_id):
     return redirect('cart_app:cart')
 
 
+def remove_cart(request,product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product,id=product_id)
+    cart_item = CartItem.objects.get(product=product,cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    
+    return redirect('cart_app:cart')
+
+
+
+def remove_cart_item(request,product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product,id=product_id)
+    cart_item = CartItem.objects.get(product=product,cart=cart)
+    cart_item.delete()
+    return redirect('cart_app:cart')
+
+
 
 def cart(request,total=0,quantity=0,cart_items=None):
     try:
@@ -45,12 +67,16 @@ def cart(request,total=0,quantity=0,cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        tax = (2*total)/100
+        grand_total = total+tax
     except ObjectDoesNotExist:
         pass  #Ignore it
 
     context = {
         'total':total,
         'quantity':quantity,
-        'cart_items':cart_items
+        'cart_items':cart_items,
+        'tax':tax,
+        'grand_total':grand_total,
     }
     return render(request,'store/cart.html',context)
